f697d0d71f49f2ece95ea7d5af846353c1cf1a9d
Rui Chicória <rchicoria@ricardo-VirtualBox.(none)>
2012-11-19 04:11:07 +0000

    project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoisEnrichmentApp::Application.config.secret_token = 'ce7b0c2467d92f170cb96c6b8f704df5b711aaec9acf87dfb1b3bc9230c2c028763ec710d13f185bdbb87cae89bee7eb5e3fbd0bfe8249928956a50d18ee6830'
