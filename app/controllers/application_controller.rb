f439a7cfd08f2daaca6cdb77c9ed685ea3ca1ae0
Ricardo Lopes <mail@ricardolopes.net>
2012-11-20 23:13:57 +0000

    Mobile interface now not showing in desktop sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def mobile_agent?
+  	# negate this statement to test for mobile
+	(request.user_agent =~ /Mobile|webOS/ and not request.user_agent =~ /iPad/)
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
 	CATEGORY_DEFAULT = 2
 
 	def index
+		redirect_to pois_url unless mobile_agent?
 		@district = DISTRICT_DEFAULT
 		@category = CATEGORY_DEFAULT
 		@district = Integer(params[:district]) if params[:district] && Integer(params[:district])
