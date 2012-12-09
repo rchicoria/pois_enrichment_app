ce982aeb228e5f98f31c763311b815a5adc0638e
Rui Pedro da Cunha Santos Chicória <rchicoria@gmail.com>
2012-12-09 18:04:40 +0000

    Constante que guarda stopwrods recebidas a partir de um ficheiro de text

--- config/initializers/app_constants.rb
+++ config/initializers/app_constants.rb
@@ -1,2 +1,7 @@
 API_URL = "https://www.ost.pt/rest/"
 API_KEY = "key=jiwvircSWWiSmrYftHEBEyCxXOqAMyqHqjrQGsjW"
+STOPWORDS_PT = get_array_from_file("stopwords_pt.txt")
+
+def get_array_from_file(filename)
+	stopwords = IO.read(filename).split
+end
